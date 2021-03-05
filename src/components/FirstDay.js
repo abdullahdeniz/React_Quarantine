3cb5410d4889376414283ede47295bb610edd00d
gelenek <abdullahdenizfr@gmail.com>
2021-03-05 10:58:43 +0100

    initial

--- src/components/FirstDay.js
+++ src/components/FirstDay.js
@@ -12,6 +12,17 @@ class FirstDay extends React.Component{
                     <h1>Hello My Blog in React</h1>
                     <p>I want do</p>
                 </div>
+
+                <div className=" col-md-8 mb-4">
+                    <div className="card">
+                        <div className="card-header d-flex justify-content-beetween">
+                            <h4 className="d-inline"> yaz </h4>
+                            
+                            <i className = "far fa-trash-alt" style = {{cursor : "pointer"}}>myke</i>
+                        </div>
+                </div>
+                
+                </div>
             </Router>
             
         )
